8f61431ddae407792d3f5245fe49c939b5feaf75
Bam <40043837+BamV@users.noreply.github.com>
2021-02-01 00:16:08 -0300

    Add files via upload

--- config/valkyrie.js
+++ config/valkyrie.js
@@ -17,4 +17,13 @@ module.exports = [
         message: '{icon} cd in {duration}',
 		time_remaining: [5, 3, 1]
 	},	
+
+	//Partial Eclipse Expiring
+       {
+         type: 'Expiring',
+         target: 'Self',
+         abnormalities: 10155533, 
+         message: '{icon} Expiring {duration} ',
+         time_remaining: [2,1]
+      },
 ]
